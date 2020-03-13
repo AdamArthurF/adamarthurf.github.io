727c35839e8965ba558c3f27b7a7f8cb681b018e
AdamArthurFaizal <adam.faizal.af6@gmail.com>
2020-03-13 20:05:00 +0700

    memperbarui web lagi dengan menambahkan jquery

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 /*
  * Copyright (c) 2020. Adam Arthur Faizal.
  */
+$(document).ready(function () {
+    $('.smooth-scroll').click(function (event) {
+        const tujuan = this.hash;
+        const navHeight = $('nav').outerHeight(true);
+        $('html, body').animate({
+            scrollTop: $(tujuan).offset().top - navHeight
+        }, 1000);
+        event.preventDefault();
+    });
 
+    $('.navbar-toggler').click(function () {
+        $('.collapse').collapse('toggle');
+    });
 
+    $('.my-gambar').click(function (event) {
+        const targetModal = '#' + this.nextElementSibling.id;
+        $(targetModal).modal();
+        event.preventDefault()
+    });
 
-$('.smooth-scroll').on('click', function (event) {
-    const tujuan = this.hash;
-    const navHeight = $('nav').height();
-    $('html, body').animate({
-        scrollTop: $(tujuan).offset().top - navHeight
-    }, 1000);
-    event.preventDefault();
+    $('#contact').mouseenter(function () {
+        $('.my-medsos').tooltip();
+    })
 });
 
