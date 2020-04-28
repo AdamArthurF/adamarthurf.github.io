2fbc72b46c8a5b6cb4e99ce0c0592c5b81d10e12
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-28 12:28:54 +0700

    Updates

---
+++ precache-manifest.1ca02fb8b718b671c6e4c4a7db758e8b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda2f2b8c9f99d75e9fad9ccd5f9114f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7610f3d55cae4a7276",
+    "url": "/static/css/main.5e9ba1ac.chunk.css"
+  },
+  {
+    "revision": "f334359907fcd6527cc0",
+    "url": "/static/js/2.1819b83d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1819b83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41654a285873ae6721a0",
+    "url": "/static/js/3.50ddbade.chunk.js"
+  },
+  {
+    "revision": "682715112543b4869165",
+    "url": "/static/js/4.a0911a23.chunk.js"
+  },
+  {
+    "revision": "812817832d546145a107",
+    "url": "/static/js/5.ca136d6a.chunk.js"
+  },
+  {
+    "revision": "4e7610f3d55cae4a7276",
+    "url": "/static/js/main.577de84b.chunk.js"
+  },
+  {
+    "revision": "a5fe68299771a31ee365",
+    "url": "/static/js/runtime-main.2ab5b5a0.js"
+  },
+  {
+    "revision": "8b055f01b73ad5dc771b2428fdecc34e",
+    "url": "/static/media/Cuprum-Regular-webfont.8b055f01.woff"
+  },
+  {
+    "revision": "ff0f52c6960fa7f5ee9bb20d8e88a686",
+    "url": "/static/media/DSC0021.ff0f52c6.JPG"
+  },
+  {
+    "revision": "d4fd8fdab02bb03463a815dfa02eae92",
+    "url": "/static/media/DSC0061.d4fd8fda.JPG"
+  },
+  {
+    "revision": "8672b338d80829d0ffed852177a363f8",
+    "url": "/static/media/IMG_2979.8672b338.JPG"
+  },
+  {
+    "revision": "aeae0f3ed093497c72a6436f0db02c49",
+    "url": "/static/media/IMG_8154.aeae0f3e.JPG"
+  },
+  {
+    "revision": "8a9391dc3b9f2f33eba871519d0c95d9",
+    "url": "/static/media/IMG_9483.8a9391dc.JPG"
+  },
+  {
+    "revision": "4dcb4aad242e4296c5ac44600401c2a3",
+    "url": "/static/media/IMG_9618.4dcb4aad.JPG"
+  },
+  {
+    "revision": "74295694cbd3cf50532fb9f55622c038",
+    "url": "/static/media/IndieFlower-webfont.74295694.woff"
+  },
+  {
+    "revision": "c72395db55dee9b284336b87d3a99974",
+    "url": "/static/media/foto_sma.c72395db.jpg"
+  },
+  {
+    "revision": "54ee10cf5d5ce058d7f8316cd241ab4a",
+    "url": "/static/media/naruto2.54ee10cf.jpg"
+  }
+]);
