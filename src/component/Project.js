7fec4d6ceace9cbedd81a987be9f698164452a0b
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-03-23 13:48:09 +0700

    Memodifikasi sesuatu dan menambahkan trafis.yml

--- src/component/Project.js
+++ src/component/Project.js
@@ -10,6 +10,7 @@ function Project() {
     return (
         <React.Fragment>
             <section className="project">
+
                 <Row>
                     <Col sm="6" className="putih p-5 d-flex flex-column align-items-center justify-content-center">
                         <h1 className="text-center font-weight-bold mb-3">Suwit Jawa Game</h1>
@@ -27,14 +28,17 @@ function Project() {
                             </a>
                         </div>
                     </Col>
+
                     <Col sm="6" className='biru p-5 d-flex justify-content-center align-items-center'>
                         <img src="img/project/suwit-jawa.png" alt="Suwit Jawa Game" className="img-fluid" width="500"/>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col sm="6" className="biru p-5 d-flex justify-content-center align-items-center">
                         <img src="img/project/whack-a-mole.png" alt="Pukul Tikus Tanah" className="img-fluid" width="500"/>
                     </Col>
+
                     <Col sm="6" className='putih p-5'>
                         <h1 className="text-center font-weight-bold mb-3">Whack a Mole</h1>
                         <p className="mb-5 text-center">
@@ -50,9 +54,9 @@ function Project() {
                                 {' '}GitHub
                             </a>
                         </div>
-
                     </Col>
                 </Row>
+
                 <Row>
                     <Col sm="6" className="putih p-5">
                         <h1 className="text-center font-weight-bold">AdamMovies | Movie Search Engine</h1>
@@ -74,10 +78,12 @@ function Project() {
                         <img src="img/project/adammovies.png" alt="Adam Movies" className="img-fluid" width="500"/>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col sm="6" className="biru p-5 d-flex justify-content-center align-items-center">
                         <img src="img/project/todoapp.png" alt="TodoApp" className="img-fluid" width="500"/>
                     </Col>
+
                     <Col sm="6" className="putih p-5">
                         <h1 className="text-center font-weight-bold">Todo App</h1>
                         <p className="mb-5 text-center">
@@ -94,30 +100,34 @@ function Project() {
                         </div>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col sm="6" className="p-5">
                         <h1 className="text-center font-weight-bold">Tic Tac Toe Game</h1>
                         <p className="mb-5 text-center">
                             Simple arcade game for children. It build with React.js </p>
                         <div className="d-flex flex-row justify-content-center align-items-center ">
-                            <a href="https://adamarthurf.github.io/TodoApp/" className="btn btn-outline-primary rounded-pill mr-3">
+                            <a href="https://adamarthurf.github.io/tictactoe/" className="btn btn-outline-primary rounded-pill mr-3">
                                 <i className="fas fa-globe-asia"/>
                                 {' '}Website
                             </a>
-                            <a href="https://github.com/adamarthurf/TodoApp/" className="btn btn-outline-danger rounded-pill">
+                            <a href="https://github.com/adamarthurf/tictactoe/" className="btn btn-outline-danger rounded-pill">
                                 <i className="fab fa-github"/>
                                 {' '}GitHub
                             </a>
                         </div>
                     </Col>
+
                     <Col sm="6" className="biru p-5 d-flex justify-content-center align-items-center">
                         <img src="img/project/tictactoe.png" alt="Tic Tac Toe" className="img-fluid" width="500"/>
                     </Col>
                 </Row>
+
                 <Row>
                     <Col sm="6" className="biru p-5 d-flex justify-content-center align-items-center">
                         <img src="img/project/adamanime.png" alt="Adam Anime" className="img-fluid" width="500"/>
                     </Col>
+
                     <Col sm="6" className='putih p-5'>
                         <h1 className="text-center font-weight-bold">AdamAnime | Anime Search Engine</h1>
                         <p className="mb-5 text-center">
@@ -134,8 +144,8 @@ function Project() {
                             </a>
                         </div>
                     </Col>
-
                 </Row>
+
                 <Row>
                     <Col sm="6" className="putih p-5">
                         <h1 className="text-center font-weight-bold">Simple SI Stasiun Kereta</h1>
@@ -153,6 +163,7 @@ function Project() {
                             </a>
                         </div>
                     </Col>
+
                     <Col sm="6" className='biru p-5 d-flex justify-content-center align-items-center'>
                         <img src="img/project/stasiun-kereta.png" alt="Stasiun Kereta" className="img-fluid" width="500"/>
                     </Col>
